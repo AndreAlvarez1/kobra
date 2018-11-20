e96a6b700c5eadb7260eca42698f0193ed737911
Andre <hey@andrealvarez.com>
2018-11-20 20:25:04 -0300

    paypal envio ok

--- app/controllers/billings_controller.rb
+++ app/controllers/billings_controller.rb
@@ -4,7 +4,38 @@ class BillingsController < ApplicationController
     @buyer = Buyer.find(params[:buyer_id])
     orders = @buyer.orders.where(status: false)
     total = orders.pluck("price*quantity").sum()
-    
+    items = orders.map do |order|
+      item = {}
+      item[:name] = order.product.name
+      item[:sku] = order.id.to_s
+      item[:price] = order.price.to_s
+      item[:currency] = 'USD'
+      item[:quantity] = order.quantity
+      item
+    end
+
+    @payment = PayPal::SDK::REST::Payment.new({
+    :intent =>  "sale",
+    :payer =>  {
+      :payment_method =>  "paypal" },
+    :redirect_urls => {
+      :return_url => "http://localhost:3000/payment/execute",
+      :cancel_url => "http://localhost:3000/" },
+    :transactions =>  [{
+      :item_list => {
+        :items => items
+      },
+      :amount =>  {
+        :total =>  total,
+        :currency =>  "USD" },
+      :description =>  "Carro de Compra KOBRA" }]})
+
+        if @payment.create
+          redirect_url = @payment.links.find{|v| v.method == "REDIRECT" }.href
+          redirect_to redirect_url
+        else
+          ':('
+        end
   end
 
 end
