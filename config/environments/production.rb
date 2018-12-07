7cfc0f96b2d05ce11d456795316bd2c03dc474e3
Andre <hey@andrealvarez.com>
2018-12-06 23:29:07 -0300

    otro arregl de vistas

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,8 @@ Rails.application.configure do
 
   config.public_file_server.enabled = true
 
+  config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif *.svg]
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
