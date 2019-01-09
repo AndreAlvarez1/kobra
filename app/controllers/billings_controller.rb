0c1c35352faba47537d5d98f96933381156c3877
Andre <hey@andrealvarez.com>
2019-01-08 22:21:02 -0300

    saque todo lo de paypal para mandar los mails sin executarlo

--- app/controllers/billings_controller.rb
+++ app/controllers/billings_controller.rb
@@ -12,43 +12,14 @@ class BillingsController < ApplicationController
     @buyer = Buyer.find(params[:buyer_id])
     orders = @buyer.orders.where(status: 0)
     total = orders.pluck("price*quantity").sum()
-    items = orders.map do |order|
-      item = {}
-      item[:name] = order.product.name
-      item[:sku] = order.id.to_s
-      item[:price] = order.price.to_s
-      item[:currency] = 'USD'
-      item[:quantity] = order.quantity
-      item
-    end
 
-    @payment = PayPal::SDK::REST::Payment.new({
-    :intent =>  "sale",
-    :payer =>  {
-      :payment_method =>  "paypal" },
-    :redirect_urls => {
-      :return_url => "http://localhost:3000/billings/execute/#{@buyer.id}",
-      :cancel_url => "http://localhost:3000/" },
-    :transactions =>  [{
-      :item_list => {
-        :items => items
-      },
-      :amount =>  {
-        :total =>  total,
-        :currency =>  "USD" },
-      :description =>  "Carro de Compra KOBRA" }]})
 
-        if @payment.create
-
-          billing = Billing.create(
+    billing = Billing.create(
           buyer: @buyer,
-          payment_id: @payment.id,
-          link: @payment.links.find{|v| v.method == "REDIRECT" }.href
           )
-
             orders.map do |order|
             order.status = 1
-            order.payment_id = @payment.id
+            order.payment_id = billing.id
             order.billing_id = billing.id
             order.save
           end
@@ -58,20 +29,16 @@ class BillingsController < ApplicationController
           respond_to do |format|
               format.html {redirect_to billings_path, notice: 'Kobro enviado con exito.'}
           end
-        else
-          ':('
-        end
   end
 
-  def rekobrar
-    @billing = Billing.find(params[:id])
-    ExampleMailer.sample_email(@billing.buyer,@billing.link,current_seller,@billing).deliver
+    def rekobrar
+      @billing = Billing.find(params[:id])
+      ExampleMailer.sample_email(@billing.buyer,@billing.link,current_seller,@billing).deliver
 
-    respond_to do |format|
-        format.html {redirect_to billings_path, notice: 'Kobro RE-enviado'}
+      respond_to do |format|
+          format.html {redirect_to billings_path, notice: 'Kobro RE-enviado'}
+      end
     end
-
-  end
 
 
   def paid
@@ -84,33 +51,6 @@ class BillingsController < ApplicationController
 
   end
 
-
-  def execute
-    @buyer = Buyer.find(params[:buyer_id])
-    paypal_payment = PayPal::SDK::REST::Payment.find(params[:paymentId])
-     if paypal_payment.execute(payer_id: params[:PayerID])
-
-       amount = paypal_payment.transactions.first.amount.total
-
-       billing = Billing.find_by(payment_id: params[:paymentId])
-       billing.update(
-       buyer: @buyer,
-       code: paypal_payment.id,
-       payment_method: 'paypal',
-       amount: amount,
-       currency: 'USD',
-       status:1
-       )
-
-       orders = @buyer.orders.where(status: 1, payment_id: params[:paymentId])
-       orders.update_all(status: 2)
-
-       redirect_to pages_success_path, notice: "La compra se realizó con éxito!"
-     else
-       render plain: "No se puedo generar el cobro en PayPal."
-     end
-
-  end
 
 
 
