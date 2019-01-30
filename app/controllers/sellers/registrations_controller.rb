43424c6f55908a7dec017bb8d5e28f614505db0f
André Alvarez <Andre@iMac-de-Andres.local>
2019-01-30 20:02:14 -0300

    modifique el controller del seller

--- app/controllers/sellers/registrations_controller.rb
+++ app/controllers/sellers/registrations_controller.rb
@@ -2,7 +2,7 @@
 
 class Sellers::RegistrationsController < Devise::RegistrationsController
    before_action :configure_sign_up_params, only: [:create]
-   before_action :configure_account_update_params, only: [:update]
+   before_action :configure_account_update_params, only: [:update, :edit]
 
   # GET /resource/sign_up
   # def new
@@ -15,14 +15,14 @@ class Sellers::RegistrationsController < Devise::RegistrationsController
    end
 
   # GET /resource/edit
-  # def edit
-  #   super
-  # end
+   def edit
+     super
+   end
 
   # PUT /resource
-  # def update
-  #   super
-  # end
+   def update
+    super
+   end
 
   # DELETE /resource
   # def destroy
