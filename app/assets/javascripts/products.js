e269162948d5dec41be907551a94a3a8b2561982
Andre Alvarez <hey@andrealvarez.com>
2019-04-02 20:47:32 -0300

    agregué datatables a productos

---
+++ app/assets/javascripts/products.js
@@ -0,0 +1,32 @@
+
+
+  document.addEventListener('turbolinks:before-cache', function(){
+      $('#productsTable').DataTable().destroy();
+  })
+
+  document.addEventListener('turbolinks:load', function(){
+    $('#productsTable').DataTable( {
+      initComplete: function () {
+
+                this.api().columns().every( function () {
+                    var column = this;
+                    var select = $('<select><option value=""></option></select>')
+                        .appendTo( $(column.footer()).empty() )
+                        .on( 'change', function () {
+                            var val = $.fn.dataTable.util.escapeRegex(
+                                $(this).val()
+                            );
+
+                            column
+                                .search( val ? '^'+val+'$' : '', true, false )
+                                .draw();
+                        } );
+
+                    column.data().unique().sort().each( function ( d, j ) {
+                        select.append( '<option value="'+d+'">'+d+'</option>' )
+                    } );
+                } );
+            }
+        } );
+
+  })
